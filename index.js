e37c2b55ef15da1d13ac3ff356fb10d7982a57eb
Simone <simoneungaro@hotmail.it>
2020-03-15 13:58:47 +0100

    testing

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+// require the discord.js module
 const Discord = require('discord.js');
+
+// create a new Discord client
 const client = new Discord.Client();
 
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
+client.on('message', message => {
+  console.log(message.content);
+  if (message.content === '!ping') {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send('Pong.');
+  }
+});
+
+// login to Discord with your app's token
 client.login('Njg4MDQxMjE0NzIwNzM3MzM0.Xm4WLQ.5CSt2DNOQOMCTTV4fjF9-PeMi2w');
