c523411abd1214191af7c7fca0f71f23ba5aecda
Simoneu01 <simoneungaro@hotmail.it>
2020-03-15 12:54:55 +0100

    Set up ESLint & init discordbot

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('Njg4MDQxMjE0NzIwNzM3MzM0.Xm4WLQ.5CSt2DNOQOMCTTV4fjF9-PeMi2w');
