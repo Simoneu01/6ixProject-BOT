c18ccb4965e5df0f988d307ed116c3e9ac7e72ef
Simone <simoneungaro@hotmail.it>
2020-03-15 15:23:03 +0100

    dynamic-commands

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-// require the discord.js module
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-// create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
-client.once('ready', () => {
-  console.log('Ready!');
-});
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.on('message', message => {
-  console.log(message.content);
-  if (message.content === '!ping') {
-    // send back "Pong." to the channel the message was sent in
-    message.channel.send('Pong.');
-  }
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		client.commands.get('ping').execute(message);
+	} else if (command === 'beep') {
+		client.commands.get('beep').execute(message);
+	}
 });
 
-// login to Discord with your app's token
-client.login('Njg4MDQxMjE0NzIwNzM3MzM0.Xm4WLQ.5CSt2DNOQOMCTTV4fjF9-PeMi2w');
+client.login(token);
