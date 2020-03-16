a75189f760df2fd3673e507ddf0348c79124f080
Simone <simoneungaro@hotmail.it>
2020-03-16 10:56:59 +0100

    fix prune

--- index.js
+++ index.js
@@ -21,18 +21,12 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
+	const command = args.shift().toLowerCase();
 
-	if (!client.commands.has(commandName)) return;
-
-	const command = client.commands.get(commandName);
-
-	if (command.args && !args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-	}
+	if (!client.commands.has(command)) return;
 
 	try {
-		command.execute(message, args);
+		client.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
