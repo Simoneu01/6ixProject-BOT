08c6a59b83eb42d536bab7d45e1c6f19d4f4c37c
Simone <simoneungaro@hotmail.it>
2020-07-23 21:53:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Esempio server HTTP\n');
+});
+
+const callback = () => {
+  const address = server.address().address;
+  const port = server.address().port;
+  console.log(`
+  Server avviato all'indirizzo http://${address}:${port}
+  `);
+}
+
+server
+  .listen(
+    80, 
+    '127.0.0.1',
+    callback
+  )
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
